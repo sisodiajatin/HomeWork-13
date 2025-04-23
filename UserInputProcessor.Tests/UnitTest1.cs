d331e6ab28c1b6673a07ec932e2f88fc5a1be8ac
Jatin Sisodia <sisodia1999@gmail.com>
2025-04-22 22:05:26 -0400

    first commit

---
+++ UserInputProcessor.Tests/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿using Xunit;
+using Moq;
+
+public class ATests
+{
+    [Fact]
+    public void f1_ShouldReturnValuePlus1()
+    {
+        
+        int input = 4;
+
+        
+        int result = A.f1(input);
+
+        
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void f2_ShouldReturnValuePlus2()
+    {
+        
+        int input = 3;
+
+        
+        int result = A.f2(input);
+
+        
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void f5_ShouldReturnCorrectDivision()
+    {
+        
+        int numerator = 10;
+        int denominator = 2;
+
+        
+        double result = A.f5(numerator, denominator);
+
+        
+        Assert.Equal(5.0, result);
+    }
+
+    [Fact]
+    public void f6_ShouldReturnValuePlus5_WhenNonNegative()
+    {
+        
+        int input = 7;
+
+        
+        int result = A.f6(input);
+
+        
+        Assert.Equal(12, result);
+    }
+
+    [Fact]
+    public void f6_ShouldThrowException_WhenNegative()
+    {
+        
+        int input = -1;
+
+        
+        var exception = Assert.Throws<Exception>(() => A.f6(input));
+        Assert.Equal("x can't be negative", exception.Message);
+    }
+
+    [Fact]
+    public void f7_ShouldAppendMoreStuff()
+    {
+        
+        string input = "test";
+
+        
+        string result = A.f7(input);
+
+        
+        Assert.Equal("test more stuff", result);
+    }
+
+    [Fact]
+    public void g1_ShouldCallInjected_f8_AndReturnResult()
+    {
+        
+        var mockIA = new Mock<IA>();
+        mockIA.Setup(m => m.f8(2)).Returns(42);
+
+        
+        int result = B.g1(2, mockIA.Object);
+
+        
+        Assert.Equal(42, result);
+    }
+}
---
+++ UserInputProcessor/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("UserInputProcessor.Tests")]
+
+public interface IA
+{
+    int f8(int value);
+}
+
+public class A : IA
+{
+    public static int f1(int x)
+    {
+        int result = x + 1;
+        return result;
+    }
+
+    internal static int f2(int x)
+    {
+        int result = x + 2;
+        return result;
+    }
+
+    protected static int f3(int x)
+    {
+        return x + 3;
+    }
+
+    private static int f4(int x)
+    {
+        return x + 4;
+    }
+
+    public static double f5(int x, int y)
+    {
+        return (double)x / y;
+    }
+
+    public static int f6(int x)
+    {
+        if (x < 0)
+        {
+            throw new Exception("x can't be negative");
+        }
+
+        return x + 5;
+    }
+
+    public static string f7(string s)
+    {
+        string modified = s + " more stuff";
+        return modified;
+    }
+
+    public int f8(int x)
+    {
+        
+        int fetched = x + 8;
+        return fetched;
+    }
+}
+
+internal class B
+{
+    internal static int g1(int x, IA a)
+    {
+        return a.f8(x);
+    }
+}
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int v1 = A.f1(1);
+        int v2 = A.f2(2);
+        double v5 = A.f5(5, 2);
+        int v6 = A.f6(3);
+        string v7 = A.f7("hello");
+
+        A aInstance = new A();
+        int v8 = B.g1(1, aInstance);
+
+        Console.WriteLine($"f1: {v1}, f2: {v2}, f5: {v5}, f6: {v6}, f7: {v7}, f8: {v8}");
+    }
+}
